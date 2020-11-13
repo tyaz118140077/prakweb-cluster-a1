1dec6998802a5a4baca3edfc4cd9ac481c95a748
Tyaz Gagaman <muhammad.118140077@student.itera.ac.id>
2020-11-13 21:07:47 +0700

    revisi modul 5 nomor 2

--- Modul 5/Latihan_No2.php
+++ Modul 5/Latihan_No2.php
@@ -1,9 +1,31 @@
 <html>
     <?php
         $array = ["lanirne", "aduh", "qifuat", "toda", "anevi", "samid", "kifuat"];
+        
+        for ($i=0; $i<count($array); $i++){
+            $target = $i;
+            for ($j=$i+1; $j<count($array); $j++){
+                $shorter = strlen($array[$i]);
+
+                if (strlen($array[$i])>strlen($array[$j])){
+                    $shorter = strlen($array[$j]);
+                }
+                for ($k=0; $k<$shorter; $k++){
+                    if ($array[$j][$k]<$array[$target][$k]){
+                        $target = $j;
+                        break;
+                    }else if ($array[$j][$k]>$array[$target][$k]){
+                        break;
+                    }
+                }  
+            }
+            $temp = $array[$i];
+            $array[$i] = $array[$target];
+            $array[$target] = $temp;
+        }
 
         echo "Hasil pengurutan : " . "<br>";
-        sort($array);
+
         foreach ($array as $a){
             echo $a . " ";
         }
